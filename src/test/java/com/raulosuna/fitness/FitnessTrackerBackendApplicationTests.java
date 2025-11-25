c26a20550209995a0895692206f44186c2662ca0
Osunita <raulosunaabadias71@gmail.com>
2025-11-25 13:12:45 +0100

    Project creation with minimums

---
+++ src/test/java/com/raulosuna/fitness/FitnessTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raulosuna.fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
