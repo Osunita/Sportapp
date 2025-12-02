078a469a25b2b7c3b6073e895d7416c12d9102d4
Osunita <raulosunaabadias71@gmail.com>
2025-12-03 00:40:42 +0100

    Controlador

---
+++ src/main/java/com/raulosuna/fitness/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.raulosuna.fitness.controller;
+
+import com.raulosuna.fitness.model.User;
+import com.raulosuna.fitness.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody User user) {
+        String token = userService.register(user);
+        return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest credentials) {
+        String token = userService.login(credentials.getEmail(), credentials.getPassword());
+        return ResponseEntity.ok(token);
+    }
+}
---
+++ src/main/java/com/raulosuna/fitness/model/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.raulosuna.fitness.model;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String email;
+    private String password;
+}
